192101a3bf92eb1b821e436d27cc28b581dcad4d
hosen <hosenja>
2016-12-26 11:26:23 +0400

    server side

--- server side/app.js
+++ server side/app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ecotrip');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,9 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
 app.use('/', index);
-
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server side/routes/index.js
+++ server side/routes/index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
-  res.render('index');
+router.get('/', function(req, res){
+	res.render('index', {title: "app"});
+});
+//////////////////////login 
+router.get('/login', function(req, res){
+	res.render('login');
+});
+
+//////////////// login admin
+router.post('/login/admin', function(req, res){
+  var name = req.body.username;
+  var password = req.body.password;
+  res.send(name + ", " + password);
+});
+///////////////// login company 
+router.post('/login/company', function(req, res){
+  res.send("goood")
+});
+router.get('/register', function(req, res){
+  res.render('register')
 });
 
 module.exports = router;
